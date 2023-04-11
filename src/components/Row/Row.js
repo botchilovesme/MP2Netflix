93569e618afc3ec7c72b6db66bb17f4e303581dc
botchilovesme <redlouiseanne@yahoo.com>
2023-04-12 02:36:31 +0800

    Add existing file

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,15 +1,42 @@
 import React, {useEffect, useState } from 'react';
+import axios from "axios";
 
-const Row = ({title,fetchUrl,isLargeROw}) => {
+const base_url = "https://image.tmdb.org/t/p/original";
+
+const Row = ({title,fetchUrl,isLargeRow}) => {
     const [movies, setMovies]= useState([]);
+    console.log("process.env.REACT_APP_API",process.env.REACT_APP_API);
     useEffect(() => {
-        const fetchData = async () => {}
-    })
-  return (
-    <div>
-        <h2>Row</h2>
-        </div>
-  )
-}
+        const fetchData = async () => {
+            const response = await axios.get(
+                `${process.env.REACT_APP_API}${fetchUrl}`
+                );
+            setMovies(response.data.results);
+            return response;
+        };
 
-export default Row
+        fetchData();
+    }, []);
+
+//   console.log("movies", movies);
+
+ const handleClick = () => {}
+
+    return (
+    <div className="row">
+        <h2>{title}</h2>
+        <div className="row-posters">
+            {movies && movies.map((movie)) => (
+                <img
+                onClick={() => handleClick(movie)}
+                key={movie.id}
+                className={`row-poster ${isLargeRow && "row-posterLarge"}`}
+                src={``}
+                />
+            ))}
+        </div>
+        </div>
+  );
+};
+
+export default Row;
